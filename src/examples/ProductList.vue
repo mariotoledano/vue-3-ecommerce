a88448856c8a06e5ecbec3ca35c52c552cb6dea9
mariotoledano <anotheranimal86@gmail.com>
2024-04-12 23:28:07 +0200

    ProductList component added

---
+++ src/examples/ProductList.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+    export default {
+        data(){
+            return{
+                products: [
+                    {name: 'Silla', price: 56},
+                    {name: 'Monitor', price:450},
+                    {name: 'Micrófono', price:120},
+                ]
+            }
+        },
+        methods:{
+
+        },
+    }
+</script>
+
+<template>
+    <ul>
+        <li v-for="product in products">
+            {{product.name}} ({{product.price}})</li>
+    </ul>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
-import Contador from './examples/ContadorOptions.vue'
+//import Contador from './examples/ContadorOptions.vue'
+import ProductList from './examples/ProductList.vue'
 
-const app = createApp(Contador)
+const app = createApp(ProductList)
 
 app.mount('#app')
