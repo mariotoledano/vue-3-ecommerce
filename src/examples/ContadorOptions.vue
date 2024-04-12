bd606380fc75ae8ea11590808ffb04f32ba89997
mariotoledano <anotheranimal86@gmail.com>
2024-04-12 23:05:43 +0200

    Agregar ejemplos básicos

---
+++ src/examples/ContadorOptions.vue
@@ -0,0 +1,25 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+    export default {
+        data(){
+            return{
+                count: 0
+            }
+        },
+        methods:{
+            increase(){
+                this.count+=1;
+            }
+        },
+        mounted() {
+            console.log('El valor inicial del contador es ' + this.count);
+        },
+    }
+</script>
+
+<template>
+    <h1>Contador: {{ count }}</h1>
+    <button @click="increase">
+        Aumentar contador
+    </button>
+</template>
