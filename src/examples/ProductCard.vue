d5e3b30e44a69ba4309bf0f108404021a12d3b15
mariotoledano <anotheranimal86@gmail.com>
2024-04-13 12:56:30 +0200

    Información de padre a hijo

---
+++ src/examples/ProductCard.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<script lang="ts">
+    export default {
+        props: ['product'],
+    }
+</script>
+
+<template>
+    <p>Nombre: {{ product.name }}</p>
+    <p>Nombre: {{ product.price }}</p>
+</template>
--- src/examples/ProductList.vue
+++ src/examples/ProductList.vue
@@ -1,24 +1,28 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script lang="ts">
-    export default {
-        data(){
-            return{
-                products: [
-                    {name: 'Silla', price: 56},
-                    {name: 'Monitor', price:450},
-                    {name: 'Micrófono', price:120},
-                ]
-            }
-        },
-        methods:{
+import ProductCard from './ProductCard.vue';
 
-        },
-    }
+export default {
+    components:{
+        ProductCard
+    },
+    data(){
+        return{
+            products: [
+                {name: 'Silla', price: 56},
+                {name: 'Monitor', price:450},
+                {name: 'Micrófono', price:120},
+            ]
+        }
+    },
+    methods:{
+
+    },
+}
 </script>
 
 <template>
-    <ul>
-        <li v-for="product in products">
-            {{product.name}} ({{product.price}})</li>
-    </ul>
+    <ProductCard 
+    v-for="p in products" 
+    :product="p"/>
 </template>
