e51a95f22ff99fc0ef62c8b21227049306887a15
mariotoledano <anotheranimal86@gmail.com>
2024-05-03 16:29:37 +0200

    Filtrar y ordenar

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,7 +1,11 @@
 <script lang="ts">
 import type { Category } from '@/model/types';
+import OrderOptions from './left/OrderOptions.vue';
 
     export default {
+        components: {
+            OrderOptions,
+        },
         data() {
             return {
                 categories: [{
@@ -21,55 +25,32 @@ import type { Category } from '@/model/types';
                     name: 'home'
                 })
             },
-            selectCategory(categoryId:number){
+            goToCategory(categoryId:number){
                 this.$router.push({
                     name:'category',
                     //params: {categoryId: categoryId}
                     params: {categoryId}
                 })
-            }
+            },
         }
     }
-
 </script>
 
 <template>
     <v-sheet rounded="lg">
         <v-list rounded="lg">
             <v-list-subheader>Categorías</v-list-subheader>
-            <v-list-item 
-                link
-                @click="clearCategory()"
-                :active="$route.name === 'home'">
+            <v-list-item link @click="clearCategory()" :active="$route.name === 'home'">
                 Todas
             </v-list-item>
-            <v-list-item
-            :active="$route.name === 'category' && Number($route.params.categoryId) === category.id"
-             v-for="category in categories" 
-            :key="category.id" 
-            link
-            @click="selectCategory(category.id)">
-            <v-list-item-title>
-                {{  category.name }}
-            </v-list-item-title>
+            <v-list-item :active="$route.name === 'category' && Number($route.params.categoryId) === category.id"
+                v-for="category in categories" :key="category.id" link @click="goToCategory(category.id)">
+                <v-list-item-title>
+                    {{ category.name }}
+                </v-list-item-title>
             </v-list-item>
-
             <v-divider class="my-2"></v-divider>
-            
-            <v-list-subheader>Orden</v-list-subheader>
-            <v-list-item
-            color="grey-lighten-4" link>
-                <v-list-item-title>
-                    Por precio
-                </v-list-item-title>       
-            </v-list-item>
-
+            <OrderOptions/>
         </v-list>
-        <v-list-item
-            color="grey-lighten-4" link>
-                <v-list-item-title>
-                    Por nombre
-                </v-list-item-title>       
-            </v-list-item>
     </v-sheet>
 </template>
--- src/stores/product.ts
+++ src/stores/product.ts
@@ -3,7 +3,7 @@ import type { Product } from '@/model/types'
 
 export const useProductsStore = defineStore('products', {
   state: () => ({
-    order: '' as string,
+    order: 'price' as string,
     categoryId: null as number|null,
     _products: [
         {
@@ -54,23 +54,51 @@ export const useProductsStore = defineStore('products', {
 }),
   getters: {
     products(state) {
-        if (!state.categoryId){
-            return state._products;
+        let products = null;
+        //Filtra
+        if (state.categoryId) {
+            products = state._products
+            .filter(p => p.categoryId === state.categoryId);
+        } else {
+            products = state._products;
         }
 
-        return state._products
-        .filter(p => p.categoryId === state.categoryId);
+        //Ordenar
+        if(state.order == ''){
+            return products;
+        }
+        if (state.order === 'price'){
+            return products.sort((a,b) => a.price - b.price);
+        }
+        if (state.order === 'priceDesc'){
+            return products.sort((a,b) => b.price - a.price);
+        }
+        if (state.order === 'name'){
+            return products.sort((a,b) => a.name.localeCompare(b.name));
+        }
+        if (state.order === 'nameDesc'){
+            return products.sort((a,b) => b.name.localeCompare(a.name));
+        }
+        else {
+            return products;
+        }
     }
   },
   actions: {
     selectCategory(categoryId: number){
         this.categoryId = categoryId;
     },
-    ordeByPrice(){
-
+    orderByPrice(){
+        this.order = 'price';
     },
-    ordeByName(){
-
+    orderByPriceDesc(){
+        this.order = 'priceDesc';
+    },
+    orderByName(){
+        this.order = 'name';
+    },
+    orderByNameDesc(){
+        this.order = 'nameDesc';
     },
   },
 })
